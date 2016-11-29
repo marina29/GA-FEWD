11710dfe9cde2786fb7e3c3c71fb15c486e5e509
marina29 <marina@MARINAs-MacBook.local>
2016-11-29 18:58:11 -0500

    d11 converter

--- d11/materials/temperature-converter/js/project.js
+++ d11/materials/temperature-converter/js/project.js
@@ -2,9 +2,9 @@ console.log("This is functioning");
 
 
 $("button").on("click", function() {
-	var celsius = $("input").val()
-	console.log(celsius)
-	var farenheit = (celsius * 1.8) + 32
-	console.log(farenheit)
-	$("#converted").text(farenheit)
+	var celsius = parseFloat($("input").val());
+	console.log(celsius);
+	var farenheit = (celsius * 1.8) + 32;
+	console.log(farenheit);
+	$("#converted").text(farenheit);
 });
