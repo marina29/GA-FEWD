b7f3c97991dcfbdaf8cfe30d8f755257f649fe3f
marina29 <marina@MARINAs-MacBook.local>
2016-12-03 13:32:45 -0500

    d14 - dropdown button

--- d14/materials/dropdown-buttons/js/project.js
+++ d14/materials/dropdown-buttons/js/project.js
@@ -3,4 +3,9 @@ console.log("This is working");
 $("button").on("click", function() {
 	var selected = $(this);
 	selected.siblings("nav").toggleClass("open");
+});
+
+$("button").on("blur", function() {
+	var selected = $(this);
+	selected.siblings("nav").removeClass("open");
 })
