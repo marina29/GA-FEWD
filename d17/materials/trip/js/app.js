575c7fa97964aacfb588731377ec671d293c5e61
marina29 <marina@MARINAs-MacBook.local>
2016-12-13 20:20:25 -0500

    d17

--- d17/materials/trip/js/app.js
+++ d17/materials/trip/js/app.js
@@ -15,3 +15,42 @@ var trip = [
     photo: "http://s2.evcdn.com/images/block/I0-001/033/766/357-7.jpeg_/waset-lisbon-2017-conference-57.jpeg"
   }
 ];
+
+// CAN DO IT THIS WAY...
+
+// $("button#first").on("click", function() {
+//   console.log("1 has been clicked");
+//   $("h1").text(trip[0]["city"]+", "+trip[0]["country"]);
+//   $("img").attr("src", trip[0]["photo"]);
+// });
+
+// $("button#second").on("click", function() {
+//   console.log("2 has been clicked");
+//   $("h1").text(trip[1]["city"]+", "+trip[1]["country"]);
+//   $("img").attr("src", trip[1]["photo"]);
+// });
+
+// $("button#third").on("click", function() {
+//   console.log("3 has been clicked");
+//   $("h1").text(trip[2]["city"]+", "+trip[2]["country"]);
+//   $("img").attr("src", trip[2]["photo"]);
+// });
+
+// OR DO IT THIS WAY - EASIER - MORE GENERAL WITH ANY NUMBER...
+
+var changeStop = function(number) {
+  var city = trip[number]["city"];
+  var country = trip[number]["country"];
+  $("h1").text(city + ", " + country);
+  $("img").attr("src", trip[number]["photo"]);
+};
+
+$("button#first").on("click", function() {
+  changeStop(0);
+});
+$("button#second").on("click", function() {
+  changeStop(1);
+});
+$("button#third").on("click", function() {
+  changeStop(2);
+});
